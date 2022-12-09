dd8b13afffc6eb5aff1acfd4895cd55d90793ac7
Ibraheem-Halasah22 <huyyyy524@gmail.com>
2022-12-10 01:30:49 +0200

    user console added - but it has a bug

--- main.c
+++ main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include "tip_fuzzy_system.h"
+#include "stdbool.h"
 
 int main() {
-    printf("Hello, World!\n");
-    printf("%f\n", tip_fuzzy_system(6, 8));
+    printf("Welcome to fuzzy tip system!!!\n");
+    while(true){
+        printf("Do you want more calculations?[y/n]");
+        char choice;
+        scanf("%c", &choice);
+        if(choice == 'y'){
+            int food, service;
+            printf("please enter the value of the food:");
+            scanf("%d", &food);
+            printf("please enter the value of the service:");
+            scanf("%d", &service);
+            printf("The tip value is %f\n", tip_fuzzy_system(food, service));
+        }
+        else break;
+
+    }
     return 0;
 }
