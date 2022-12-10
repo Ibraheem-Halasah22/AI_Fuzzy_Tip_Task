d08fb0ec854bcd3e15dbc2f288bfbe006befccb2
mohamadw22 <mohamadw22@hotmail.com>
2022-12-10 21:03:22 +0200

    scanf fixed , empty buffers

--- main.c
+++ main.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
+//#include
 #include "tip_fuzzy_system.h"
 #include "stdbool.h"
 
 int main() {
+    bool x=true;
+    char choice;
+    int food, service;
     printf("Welcome to fuzzy tip system!!!\n");
-    while(true){
+    do{
         printf("Do you want more calculations?[y/n]");
-        char choice;
-        scanf("%c", &choice);
-        if(choice == 'y'){
-            int food, service;
-            printf("please enter the value of the food:");
-            scanf("%d", &food);
-            printf("please enter the value of the service:");
-            scanf("%d", &service);
-            printf("The tip value is %f\n", tip_fuzzy_system(food, service));
-        }
-        else break;
 
-    }
+        scanf(" %c", &choice);
+        if(choice == 'y'){
+
+            printf("please enter the value of the food:");
+            scanf(" %d", &food);
+            printf("please enter the value of the service:");
+            scanf(" %d", &service);
+            printf("\n");
+            printf("The tip value is %f\n", tip_fuzzy_system(food, service));
+        } else x=false;
+    }while(x);
     return 0;
 }
